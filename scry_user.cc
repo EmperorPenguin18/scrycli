35d4e0b41da64850748749a0368db58b99ce38e0
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-10-28 21:59:51 -0400

    Update scry_user.cc

--- scry_user.cc
+++ scry_user.cc
@@ -15,8 +15,13 @@ int main(int argc, char **argv)
   destroy = (void (*)(Scry*))dlsym(handle, "destroy_object");
 
   Scry* scry = (Scry*)create();
+  vector island_search = scry->cards_search("commander:g");
+  cout << island_search[5000]->name() << endl;
   Card * island = scry->cards_named("island");
   cout << island->getName() << endl;
-  scry->cleanup();
-  destroy( scry );
+  vector prime = scry->cards_autocomplete("prime");
+  cout << prime[0] << endl;
+  Card * random = scry->cards_random();
+  cout << random->name() << endl;
+  destroy(scry);
 }
